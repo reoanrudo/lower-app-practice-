575a1a2747e4249082ba43990707fb84d2291d7f
reoanrudo <sodapon03@gmail.com>
2025-11-30 17:45:00 +0900

    FizzBuzzアプリ二回目

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -4,29 +4,33 @@ use function Livewire\Volt\{state};
 
 state([
     'count' => 0,
+    'label' => 0,
 ]);
 
 $increment = function () {
-        $this->count++;
+    $this->count++;
+
+    $label = $this->count;
+
+    if ($this->count % 3 === 0 && $this->count % 5 === 0) {
+        $label = 'FizzBuzz';
+    } elseif ($this->count % 3 === 0) {
+        $label = 'Fizz';
+    } elseif ($this->count % 5 === 0) {
+        $label = 'Buzz';
+    }
+
+    $this->label = $label;
 };
 
 ?>
 
 <div>
     <h1>カウンター</h1>
-    <div>Count: {{ $count }}</div>
+    <div>
+        Count:
+        {{ $label }}
+    </div>
 
     <button wire:click="increment">+</button>
-
-    <div>
-        @if ($count !== 0)
-            @if ($count % 3 === 0 && $count % 5 === 0)
-                FizzBuzz
-            @elseif ($count % 3 === 0)
-                Fizz
-            @elseif ($count % 5 === 0)
-                Buzz
-            @endif
-        @endif
-    </div>
 </div>
